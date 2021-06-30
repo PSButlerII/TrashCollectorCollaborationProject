085bd342524bbbdece9510350ac0fd5dd1835ed0
Preston Stewart Butler II <preston.butler@live.com>
2021-06-30 12:20:14 -0400

    Uploading starter code to repo

---
+++ trash_collector/customers/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+# Create your models here.
+
+# TODO: Finish customer model by adding necessary properties to fulfill user stories
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
---
+++ trash_collector/customers/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Customer
+# Create your views here.
+
+# TODO: Create a function for each path created in customers/urls.py. Each will need a template as well.
+
+
+def index(request):
+    # The following line will get the logged-in in user (if there is one) within any view function
+    user = request.user
+    # It will be necessary while creating a customer/employee to assign the logged-in user as the user foreign key
+    # This will allow you to later query the database using the logged-in user,
+    # thereby finding the customer/employee profile that matches with the logged-in user.
+    print(user)
+    return render(request, 'customers/index.html')
