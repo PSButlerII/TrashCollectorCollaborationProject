8949d5d350509d76ea981be3e033f959129b0269
murraymilton <murray.l.milton@outlook.com>
2021-07-04 20:36:21 +0200

    Made revisions to customers views.py for added customer forms

--- trash_collector/customers/forms.py
+++ trash_collector/customers/forms.py
@@ -7,7 +7,8 @@ class customer_forms(forms.ModelForm):
         model = Customer
         fields = ('name', 'customer_address', 'customer_zip_code', 'weekly_pickup_day',)
 
+
 class change_pickup_form(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ('weekly_pick-up_day',)
+        fields = ('weekly_pickup_day',)
--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -2,8 +2,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group
 from .models import Customer
-from . forms import customer_forms, change_pickup_form
+from .forms import customer_forms, change_pickup_form
 from django.urls import reverse
+
+
+# Create your views here.
+
 # TODO: Create a function for each path created in customers/urls.py. Each will need a template as well.
 
 
@@ -13,24 +17,26 @@ def index(request):
     # It will be necessary while creating a customer/employee to assign the logged-in user as the user foreign key
     # This will allow you to later query the database using the logged-in user,
     # thereby finding the customer/employee profile that matches with the logged-in user.
-    print(user)
     return render(request, 'customers/index.html')
 
 
 def customer_signup(request):
-    user_id = request.user.id
-    form = customer_forms(request.POST)
-    if form.is_valid():
-        form.save()
-        return redirect('/customers/')
-    context = {
-        'form': form
-    }
-    return render(request, "customers/customer_signup_information.html", context)
+    if request.method == 'POST':
+        customer_name = request.POST.get('name')
+        customer_address = request.POST.get('customer_address')
+        customer_zip_code = request.POST.get('customer_zip_code')
+        customer_weekly_pickup_day = request.POST.get('weekly_pickup_day')
+        new_customer = Customer(name=customer_name, customer_address=customer_address, customer_zip_code=customer_zip_code, weekly_pickup_day=customer_weekly_pickup_day, user=request.user)
+        new_customer.save()
+
+        return HttpResponseRedirect(reverse('customers:customer_signup'))
+    else:
+        return render(request, 'customers/customer_signup_information.html')
 
 
-def customer_account_info(request, customer_pk):
-    customer = Customer.objects.get(pk=customer_pk)
+def customer_account_info(request):
+    user = request.user
+    customer = Customer.objects.get(user_id=user.id)
     form = customer_forms(request.POST, instance=customer)
     if form.is_valid():
         form.save()
@@ -40,16 +46,40 @@ def customer_account_info(request, customer_pk):
     }
     return render(request, "customers/account_info.html", context)
 
+
 def change_pickup_day(request, customer_id):
     customer = Customer.objects.get(pk=customer_id)
     form = change_pickup_form(request.POST, instance=customer)
     if form.is_valid():
         form.save(customer)
-        return redirect('/customer')
+        return redirect('/customers/')
     context = {
         'form': form
     }
     return render(request, "customers/customer_change_pickup_day.html", context)
 
+def suspend_account(request, customer_id):
+    pass
 
-# TODO: Create a function for each path created in customers/urls.py. Each will need a template as well.
+"""
+def customer_account_info(request):
+    user = request.user
+    customer = Customer.objects.filter(user_id=user.id)
+    context = {
+        'customer': customer
+    }
+    return render(request, "customers/account_info.html", context)
+"""
+
+
+"""
+user = request.user
+form = customer_forms(request.POST)
+if form.is_valid():
+    form.save()
+    return redirect('/customers/')
+context = {
+    'form': form
+}
+return render(request, "customers/customer_signup_information.html", context)
+"""
