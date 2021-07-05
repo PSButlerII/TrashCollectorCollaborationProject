5a597aeb1d463e6bf376c9f7257b3b1787ce3d3d
Preston Stewart Butler II <preston.butler@live.com>
2021-07-05 15:28:16 -0400

    Customer registration and customer information links and html files working and viewable

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -7,13 +7,13 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
-    weekly_pickup_day = models.CharField(max_length=50)
+    weekly_pickup_day = models.CharField(max_length=50, blank=True, default='')
     onetime_pickup_date = models.DateField(null=True, blank=True)
     start_suspension_date = models.DateField(null=True, blank=True)
     end_suspension_date = models.DateField(null=True, blank=True)
-    customer_balance = models.IntegerField(default=0)
-    customer_address = models.CharField(max_length=50)
-    customer_zip_code = models.CharField(max_length=50)
+    customer_balance = models.IntegerField(default=0,)
+    customer_address = models.CharField(max_length=50, blank=True, default='')
+    customer_zip_code = models.CharField(max_length=50, blank=True, default='')
     weekly_pickup_confirmed = True
     suspension = False
 
