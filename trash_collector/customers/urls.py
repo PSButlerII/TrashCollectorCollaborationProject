3b8eaee41cbd8ec45e037654bc6b215c4e887cbd
murraymilton <murray.l.milton@outlook.com>
2021-07-08 10:26:15 +0200

    Made revisions to customer urls

--- trash_collector/customers/urls.py
+++ trash_collector/customers/urls.py
@@ -5,10 +5,16 @@ from . import views
 app_name = "customers"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('registration/', views.registration, name="registration"),
-    path('detail/<int:user_id>', views.detail, name="detail"),
-    path('change/<int:user_id>/', views.change, name="change"),
-    path('pickup/<int:user_id>/', views.pickup, name="pickup"),
-    path('suspension/<int:user_id>/', views.suspension, name="suspension"),
-    path('statement/<int:user_id>/', views.statement, name="statement")
+    path('registration/',
+         views.registration, name="registration"),
+    path('detail/<int:user_id>',
+         views.detail, name="detail"),
+    path('change/<int:user_id>/',
+         views.change, name="change"),
+    path('pickup/<int:user_id>/',
+         views.pickup, name="pickup"),
+    path('suspension/<int:user_id>/',
+         views.suspension, name="suspension"),
+    path('statement/<int:user_id>/',
+         views.statement, name="statement")
 ]
